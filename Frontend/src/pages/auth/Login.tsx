4b3548b053ae14e8e92012e5f22eb331b2bc490a
neerajadhav <adhavneeraj9500@gmail.com>
2025-06-24 17:18:08 +0530

    darkmode toggle in auth pages

--- Frontend/src/pages/auth/Login.tsx
+++ Frontend/src/pages/auth/Login.tsx
@@ -1,13 +1,14 @@
-import { Badge } from "@/components/ui/badge"
-import { Link } from "react-router-dom"
-import { LoginForm } from "@/components/login-form"
-import { Target } from "lucide-react"
+import { Badge } from "@/components/ui/badge";
+import { Link } from "react-router-dom";
+import { LoginForm } from "@/components/login-form";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Target } from "lucide-react";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
+        <div className="flex justify-between w-full gap-2">
           <Link to="/" className="flex items-center space-x-2 group">
             <div className="w-8 h-8 bg-gradient-to-br from-primary to-chart-1 rounded-lg flex items-center justify-center group-hover:scale-105 transition-transform">
               <Target className="h-4 w-4 text-primary-foreground" />
@@ -15,10 +16,14 @@ export default function LoginPage() {
             <span className="text-2xl font-bold bg-gradient-to-r from-primary to-chart-1 bg-clip-text text-transparent">
               GoalForge
             </span>
-            <Badge variant="secondary" className="text-xs hidden sm:inline-flex">
+            <Badge
+              variant="secondary"
+              className="text-xs hidden sm:inline-flex"
+            >
               Beta
             </Badge>
           </Link>
+          <ModeToggle />
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
@@ -34,5 +39,5 @@ export default function LoginPage() {
         />
       </div>
     </div>
-  )
+  );
 }
